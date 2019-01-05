6d13231a579e4d101e23f609c3542acf0c4cf5fd
wangzhen <wangzhen@curefun.com>
2019-01-05 11:55:39 +0800

    fix cell tap crash

--- WZSpecialKit/Classes/Special(专题)/Detail/FFSpecialDetailController.m
+++ WZSpecialKit/Classes/Special(专题)/Detail/FFSpecialDetailController.m
@@ -22,7 +22,10 @@
     self.title = @"专题详情";
     self.view.backgroundColor = [UIColor greenColor];
     
-    self.detailView = [[NSBundle mainBundle] loadNibNamed:@"FFSecialDetailView" owner:self options:nil].lastObject;
+    //self.detailView = [[NSBundle mainBundle] loadNibNamed:@"FFSecialDetailView" owner:self options:nil].lastObject;
+    NSBundle *xibBundle = [NSBundle bundleForClass:[self class]];
+    self.detailView = [xibBundle loadNibNamed:@"FFSecialDetailView" owner:self options:nil].lastObject;
+    
     [self.view addSubview:self.detailView];
 }
 
